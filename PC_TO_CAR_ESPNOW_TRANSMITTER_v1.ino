4259126e4d1594d9d113bb333ac26301e49bdee2
BigBob5 <112042358+BigBob5@users.noreply.github.com>
2026-06-20 13:18:40 -0700

    Added comments

--- PC_TO_CAR_ESPNOW_TRANSMITTER_v1.ino
+++ PC_TO_CAR_ESPNOW_TRANSMITTER_v1.ino
@@ -1,39 +1,40 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
+//MAC address of the car receiver
 uint8_t broadcastAddress[] = {0x84, 0x1F, 0xE8, 0x38, 0xC3, 0x70};
 
-struct PacketData {
+//structure of the data packet to send
+struct PacketData { 
   int steerValue;
   int powerValue;
   int driveMode;
 };
 
-
-PacketData myData;
-
+PacketData myData; 
 esp_now_peer_info_t peerInfo;
-
 unsigned long lastPacketTime = 0;
 
-void OnDataSent(const wifi_tx_info_t *tx_info, esp_now_send_status_t status) { 
-  Serial.print("\r\nLast Packet Send Status:\t"); 
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail"); 
+//the callback function that ESP-NOW runs
+void OnDataSent(const wifi_tx_info_t *tx_info, esp_now_send_status_t status) {
+  Serial.print("\r\nLast packet status:\t"); 
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Fail"); 
 } 
 
  
 void setup() {
   Serial.begin(115200);
- 
   WiFi.mode(WIFI_STA);
-
+  
+  //initialize ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
   esp_now_register_send_cb(OnDataSent); 
-  
+
+  //set the receiver as a peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;  
   peerInfo.encrypt = false;
@@ -47,19 +48,22 @@ void setup() {
 void loop() {
   bool shouldSend = false;
 
+  //Check if new data from Python came over Serial
   if(Serial.available()>0){
     int steer=Serial.parseInt();
     int power=Serial.parseInt();
-    int gear=Serial.parseInt();
+    int gear=Serial.parseInt(); 
 
     if(Serial.read() == '\n') {
+      //It is a full packet, so save values
       myData.steerValue=steer;
       myData.powerValue=power;
       myData.driveMode=gear;
       lastPacketTime=millis();
-      shouldSend=true;
+      shouldSend=true; // send this packet
     }
   }
+  //timeout failsafe: If we havent received new data for 500ms, stop car
   if(millis()-lastPacketTime > 500) {
     if(myData.powerValue != 0 || myData.steerValue != 90) {
       myData.steerValue = 90;
@@ -68,12 +72,12 @@ void loop() {
       shouldSend = true;
     }
   }
-  if(shouldSend){ 
-   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+  //send packet
+  if(shouldSend){
+   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData)); // send packet
     if (result == ESP_OK) {
       Serial.println("Sent with success");
-    }
-    else {
+    } else {
       Serial.println("Error sending the data");
     }
   }
